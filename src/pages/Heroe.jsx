216684abb972aee271567aa51bfbc88b60d25e63
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-24 16:35:10 -0300

    will create the pages

---
+++ src/pages/Heroe.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import HeoroeCard from "../components/HeroeCard";
+
+
+const Heroe = () => {
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <div className="heroe-page" >
+
+            <h3>hero</h3>
+        </div>
+    )
+}
+
+export default Heroe
---
+++ src/pages/Search.jsx
@@ -0,0 +1,7 @@
+const Search = () => {
+    return (
+        <h1>Search</h1>
+    )
+}
+
+export default Search
