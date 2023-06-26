2eef3090efa2d7b5dd154bad5ebc45e543e762d7
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-26 19:13:44 -0300

    will create rules to search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import { NavLink } from "react-router-dom";
-
+import { NavLink, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 
 import "./Navbar.css";
+
 const Navbar = () => {
+    
+    const [search, setSearch] = useState("");
+    const param_search = `nameStartsWith=${search}`;
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        navigate(`/search?${param_search}`);
+        setSearch("");
+
+    }
+
     return (
         <nav className="nav_container">
             <NavLink
@@ -12,8 +27,13 @@ const Navbar = () => {
                 <h1>Marvel Heroes</h1>
             </NavLink>
 
-            <form className="nav_form">
-                <input type="text" placeholder="pesquise um herói" />
+            <form onSubmit={handleSubmit} className="nav_form">
+                <input
+                    type="text"
+                    placeholder="pesquise um herói"
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={search}
+                />
                 <button type="submit"><BsSearch /></button>
             </form>
         </nav>
