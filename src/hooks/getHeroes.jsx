78c0a6ba36b1300b47b1acc8dc1d1ab4d20cee6b
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-26 19:07:23 -0300

    will config environment variables

--- src/hooks/getHeroes.jsx
+++ src/hooks/getHeroes.jsx
@@ -3,14 +3,13 @@ import axios from "axios";
 //import keys from "./keys";
 import md5 from "md5";
 
+const public_key = import.meta.env.VITE_API_PUBLIC_KEY;
+const private_key = import.meta.env.VITE_API_PRIVATE_KEY;
+const base_url = "http://gateway.marvel.com/v1/public/characters";
+
 const time_stamp = new Date().getTime();
-
-const public_key = "0c36393870e115e681152a5959de86b8";
-const private_key = "aa2e0be558a6e4569cf62be92b4197e1930481e4";
 const hash = md5(time_stamp + private_key + public_key);
-const base_url = "http://gateway.marvel.com/v1/public/";
 const url = `${base_url}?ts=${time_stamp}&apikey=${public_key}&hash=${hash}`;
-
 
 const getHeroes = axios.create({
     baseURL: `${url}`
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,28 +2,28 @@ import { useEffect, useState } from "react";
 import getHeroes from "../hooks/getHeroes";
 import md5 from "md5";
 //import keys from "../hooks/keys";
+import HeroeCard from "../components/HeroeCard";
+
+import "./Home.css";
+
+const public_key = import.meta.env.VITE_API_PUBLIC_KEY;
+const private_key = import.meta.env.VITE_API_PRIVATE_KEY;
+const base_url = "http://gateway.marvel.com/v1/public/characters";
 
 const time_stamp = new Date().getTime();
-
-const public_key = "0c36393870e115e681152a5959de86b8";
-const private_key = "aa2e0be558a6e4569cf62be92b4197e1930481e4";
 const hash = md5(time_stamp + private_key + public_key);
-const base_url = "http://gateway.marvel.com/v1/public/characters";
 const url = `${base_url}?ts=${time_stamp}&apikey=${public_key}&hash=${hash}`;
 
-
-import "./Home.css"
-import HeroeCard from "../components/HeroeCard";
 const Home = () => {
     const [heroes, setHeroes] = useState([]);
 
-async function LoadHeroes(){
-            const response = await getHeroes.get(`${url}`);
-            //console.log(response.data.data.results)
-            setHeroes(response.data.data.results)
-        }
+    async function LoadHeroes() {
+        const response = await getHeroes.get(`${url}`);
+        //console.log(response.data.data.results)
+        setHeroes(response.data.data.results)
+    }
     useEffect(() => {
-        LoadHeroes();
+        //LoadHeroes();
     }, [])
 
     return (
@@ -31,10 +31,10 @@ async function LoadHeroes(){
             <h2>Saiba mais sobre seus heróis favoritos!</h2>
             <div className="home_card">
                 {heroes.length === 0 && <p>carregando...</p>}
-                {heroes.map(character => <HeroeCard key={character.id} character={ character} />
-        
+                {heroes.map(character => <HeroeCard key={character.id} character={character} />
+
                 )}
-            </div>    
+            </div>
         </div>
     )
 }
