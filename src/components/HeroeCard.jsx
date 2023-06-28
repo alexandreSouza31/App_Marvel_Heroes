44ae99a6b2fdeb6ec7a23d5fa469689165ab897a
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-28 12:10:51 -0300

    will create a new property to set

--- src/components/HeroeCard.jsx
+++ src/components/HeroeCard.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 
-const HeroeCard = ({ character, showLink = true }) => {
+const HeroeCard = ({ character,character_name=true, showLink = true }) => {
     return (
         <div className="container_heroe_card">
             <div key={character.id} className="img_container">
                 <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt={character.name} />
             </div>
             <div className="link_heroe">
-                <h3>{character.name}</h3>
+                {character_name &&
+                    <h3>{character.name}</h3>
+                }
                 {showLink &&
                     <Link to={`/heroe/${character.id}`}>{/*mesmo estilo da url do main.jsx*/}
                         Ver mais
