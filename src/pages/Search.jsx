e1d0f069579b6e5d9546df5d3f86570d5b7d327d
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-28 00:19:15 -0300

    will delete stale lines.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,25 +2,28 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 import md5 from "md5";
-import HeroeCard from "../components/HeroeCard"
+import HeroeCard from "../components/HeroeCard";
 import getHeroes from "../hooks/getHeroes";
+
+const public_key = import.meta.env.VITE_API_PUBLIC_KEY;
+const private_key = import.meta.env.VITE_API_PRIVATE_KEY;
+const base_url = "http://gateway.marvel.com/v1/public/characters";
+
+const time_stamp = new Date().getTime();
+const hash = md5(time_stamp + private_key + public_key);
 
 const Search = () => {
     const [searchParams] = useSearchParams();
     const [heroes, setHeroes] = useState([]);
     const results = searchParams.get("nameStartsWith");
-
-    const public_key = import.meta.env.VITE_API_PUBLIC_KEY;
-    const private_key = import.meta.env.VITE_API_PRIVATE_KEY;
-    const base_url = "http://gateway.marvel.com/v1/public/characters";
-
-    const time_stamp = new Date().getTime();
-    const hash = md5(time_stamp + private_key + public_key);
+    
     const url = `${base_url}?nameStartsWith=${results}&ts=${time_stamp}&apikey=${public_key}&hash=${hash}`;
 
     async function findHeroes() {
-        const response = await getHeroes.get(`${url}`);
+        const response = await getHeroes.get(`${url}`)
+        
         setHeroes(response.data.data.results);
+       
     }
     useEffect(() => {
         findHeroes();
