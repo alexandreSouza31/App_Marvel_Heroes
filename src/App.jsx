40d993b8b35b5045b3109ac5cacefd003d1d22f0
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-07-03 00:35:13 -0300

    will create the styled-component to Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <div>
-      <Navbar />
+      <Header />
       <Outlet/>
     </div>
   )
---
+++ src/components/Styled-components/HeaderStyles.jsx
@@ -0,0 +1,56 @@
+import styled from "styled-components"
+
+export const HeaderStyles = styled.header`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-wrap: wrap;
+padding: 2rem .5rem !important;
+width: 100%;
+min-height: 15vh;
+background-color: #E61C23;
+
+& h1 {
+    color: whitesmoke;
+    font-size: 2.5rem;
+}
+
+& form {
+    display: flex;
+    align-items: center;
+}
+
+& button {
+    font-size: .8rem;
+    border-radius: 8px;
+    border: none;
+    padding: 0.4em .8em;
+    margin-left: .5rem;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: #1a1a1a;
+    cursor: pointer;
+    color: whitesmoke;
+    border: 1px solid whitesmoke;
+    transition: .4s;
+}
+
+& button:hover{
+    color: #E61C23;
+}
+
+@media screen and (max-width:360px) {
+        min-height: 20vh;
+
+    & h1 {
+        font-size: 2rem;
+    }
+}
+
+@media screen and (min-width:650px) {
+        justify-content: space-between;
+        min-height: 10vh;
+    
+}
+`
